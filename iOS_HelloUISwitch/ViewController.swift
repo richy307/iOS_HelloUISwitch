3f33fd035e17d79fa3f4c2819a6add293f71e66c
Richy <richy@webcomm.com.tw>
2023-07-17 20:13:26 +0800

    new a UISwitch with code

--- iOS_HelloUISwitch/ViewController.swift
+++ iOS_HelloUISwitch/ViewController.swift
@@ -27,6 +27,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         mySwitch.isOn = false
         makeAChange(mySwitch)
+        
+        
+        // new a switch using code
+        let codeSwitch = UISwitch(frame: CGRect(x: view.frame.midX - 51/2,
+                                                y: view.frame.maxY-100,
+                                                width: 51,
+                                                height: 31))
+        // 加到畫面上
+        view.addSubview(codeSwitch)
+        codeSwitch.isOn = true
+        
+        // Associates a target object and action method with the control.
+        // 宣告 觸發事件
+        codeSwitch.addTarget(self, action: #selector(ViewController.codeSwitchChanges(_:)), for: .valueChanged)
+        
     }
 
 
